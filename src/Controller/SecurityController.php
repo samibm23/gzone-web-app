ca1d3f1b4d8516beafe33aac2262bae68ef5084e
mahdisoussi <61589253+mahdisoussi@users.noreply.github.com>
2022-04-25 02:10:08 +0100

    commit

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,12 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Users;
 use Prophecy\Argument\Token\TokenInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-
+use App\Repository\UsersRepository;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 class SecurityController extends AbstractController
 {
     /**
@@ -45,6 +52,116 @@ class SecurityController extends AbstractController
     public function index(): Response
     {
         return $this->render('security/login_denied.html.twig');
+    }
+     /**
+     * @Route("/forgotten-password", name="app_forgotten_password")
+     */
+    public function forgottenPass(Request $request, UsersRepository $userRepo,TokenGeneratorInterface $tokenGenerator,   MailerInterface $mailer): Response
+    {
+        // create form to retrieve email
+        $form = $this->createForm(ResetPassType::class);
+
+        //trait form
+        $form->handleRequest($request);
+
+
+        //if form is valid
+        if ( $form->isSubmitted() && $form->isValid()){
+            //extract data of the email to reset the password of
+            $data = $form->getData();
+            //search if the user exist with that email
+            $user = $userRepo->findOneBy([
+                'email' => $data['email']
+            ]);
+            //if user doesn't exist
+            if ( !$user ){
+                // we send a flash message
+                $this->addFlash('danger' , 'this email doesnt exist');
+                return $this->redirectToRoute('app_login');
+            }
+
+            // if user exist we generate a token
+            $token = $tokenGenerator->generateToken();
+            try{
+                $user->setResetToken($token);
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($user);
+                $entityManager->flush();
+            }catch(\Exception $e){
+                $this->addFlash('warning' , 'an error occured : ' . $e->getMessage());
+                return $this->redirectToRoute('app_login');
+            }
+
+            // generate url or resetting the password
+            $url = $this->generateUrl('app_reset_password' , ['token' => $token], UrlGeneratorInterface::ABSOLUTE_URL);
+            $email = (new TemplatedEmail())
+                ->from('gamex2022@gmail.com')
+                ->to($user->getEmail())
+                ->subject('PASSWORD RESET')
+                ->htmlTemplate('security/reset_email.html.twig')
+                ->context([
+                    'fullname' => $user->getFirstname() . " " .$user->getLastname(),
+                    'url' => $url,
+                ]);
+
+            $mailer->send($email);
+
+
+            //flash message of email sent confirmation
+            $this->addFlash('message', 'The reset Password email has been sent');
+            return $this->redirectToRoute('app_login');
+        }
+        // send to email request page
+        return $this->render('security/forgotten_password.html.twig', ['emailForm' => $form->createView()]);
+    }
+    
+    /**
+     * @Route("/resetPassword/{token}", name="app_reset_password")
+     */
+    public function verifyUserEmail($token,Request $request,UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        // search for user with the token
+        $user = new Users();
+        $user = $this->getDoctrine()->getRepository(User::class)->findOneBy(['reset_token' =>$token]);
+
+        if ( !$user ){
+            $this->addFlash('danger' , 'token not existing');
+            return $this->redirectToRoute('app_login');
+        }
+
+        if ( $request->isMethod('POST')){
+            $Password = $request->request->get('password');
+            $Password2 = $request->request->get('password2');
+            $error = "PASSWORD DOESNT MATCH ";
+            if ( $Password != $Password2){
+                return $this->render('security/reset_password.html.twig', [
+                    'token' => $token,
+                    'error' => $error
+                ]);
+            }
+
+            //delete token of the user
+            $user->setResetToken(null);
+            //encode password
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $request->request->get('password')
+                )
+            );
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('message' , 'password has been updated successfully');
+            return $this->redirectToRoute('app_login');
+        }
+        else{
+            return $this->render('security/reset_password.html.twig', [
+                'token' => $token,
+                'error' => ""
+            ]);
+        }
     }
 
     /**
---
+++ src/Security/EmailVerifier.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Security;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\User\UserInterface;
+use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
+use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
+
+class EmailVerifier 
+{
+    private $verifyEmailHelper;
+    private $mailer;
+    private $entityManager;
+
+    public function __construct(VerifyEmailHelperInterface $helper, MailerInterface $mailer, EntityManagerInterface $manager)
+    {
+        $this->verifyEmailHelper = $helper;
+        $this->mailer = $mailer;
+        $this->entityManager = $manager;
+    }
+
+    public function sendEmailConfirmation(string $verifyEmailRouteName, UserInterface $user, TemplatedEmail $email): void
+    {
+        $signatureComponents = $this->verifyEmailHelper->generateSignature(
+            $verifyEmailRouteName,
+            $user->getId(),
+            $user->getEmail()
+        );
+
+        $context = $email->getContext();
+        $context['signedUrl'] = $signatureComponents->getSignedUrl();
+        $context['expiresAtMessageKey'] = $signatureComponents->getExpirationMessageKey();
+        $context['expiresAtMessageData'] = $signatureComponents->getExpirationMessageData();
+        $context['fullName'] = $user->getFullName;
+
+
+        $email->context($context);
+
+        $this->mailer->send($email);
+    }
+
+    
+}
