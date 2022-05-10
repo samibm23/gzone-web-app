fb32925a87c56e63376f1ee570f63b6f7a84ad4b
mahdisoussi <61589253+mahdisoussi@users.noreply.github.com>
2022-05-10 07:23:12 +0100

    commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpClient\HttpClient;
 
 #[Route('/home')]
 class HomeController extends AbstractController
@@ -14,6 +15,16 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home_index', methods: ['GET'])]
     public function index(EntityManagerInterface $entityManager): Response
     {
+
+        $httpClient = HttpClient::create(['headers' => [
+            'X-RapidAPI-Host' => 'rawg-video-games-database.p.rapidapi.com',
+            'X-RapidAPI-Key' => 'e5a466d3efmsh7027ddb554dd829p14c940jsne6021b74bb7b'
+        ]]);
+    
+
+        $response = $httpClient->request('GET', 'https://rawg-video-games-database.p.rapidapi.com/games/7f47c38abab24b96be4c93f4ac1627fc');
+        echo $response->getContent() . "\n";
+
         return $this->render('home/index.html.twig');
     }
 }
